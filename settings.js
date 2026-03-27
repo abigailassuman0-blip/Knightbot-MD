afbf8b7d4bb4ac0f00d36abdf733c74ac7d060bb
abigailassuman0-blip <abigailassuman0@gmail.com>
2026-03-27 22:00:51 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Nana Ama', // Your name
-  ownerNumber: '233271779275', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+233271779275', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   maxStoreMessages: 20, 
